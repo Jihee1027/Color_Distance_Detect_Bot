97f53f55619fcce79a106d0d94d43d7e5284763b
bpatke <bpatke@purdue.edu>
2026-04-21 17:38:15 -0400

    Update pwm.h

--- include/pwm/pwm.h
+++ include/pwm/pwm.h
@@ -2,8 +2,8 @@
 #define PWM_H
 
 void init_pwm(void);
-void set_right_motor_speed(double speed);
-void set_left_motor_speed(double speed);
+void set_right_motor_speed(float speed);
+void set_left_motor_speed(float speed);
 int areMotorsOn(void);
 
 #endif
